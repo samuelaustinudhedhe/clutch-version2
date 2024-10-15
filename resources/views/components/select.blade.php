9100b84c1ad306d213909ff7434d216f548dc2d9
SAU <samuelaustinudhedhe@gmail.com>
2024-10-15 22:54:41 +0100

    Fix x-select js Conflicking with carousel

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -11,7 +11,26 @@
                 const defaultValue = select.getAttribute('data-default-value');
 
                 Array.from(select.attributes).forEach(attr => {
-                    
+                    if (attr.name.startsWith('wire:model')) {
+
+                        // Use Livewire's @this.get() to check if the model has a value
+                        if (!@this.get(`${attr.value}`)) {
+                            if (defaultValue) {
+                                @this.set(`${attr.value}`, defaultValue);
+                                select.value = defaultValue; // Set the select element's value to defaultValue
+                            } else {
+                                // Loop through options to find the one with the 'selected' attribute
+                                let defaultOption = Array.from(select.options).find(option => option.hasAttribute('selected'));
+                                if (defaultOption.value) {
+                                    @this.set(`${attr.value}`, defaultOption.value);
+                                    select.value = defaultOption.value; // Set the select element's value to the default option
+                                } else {
+                                    // If select.value is not empty, use it as the default
+                                    @this.set(`${attr.value}`, select.value);
+                                }
+                            }
+                        }
+                    }
                 });
             });
         }
