05abad3bcc7099193a0860a51f1243293bb8f340
SAU <samuelaustinudhedhe@gmail.com>
2024-12-30 13:06:54 +0100

    Refactor payment routes and remove unused controller
    - Deleted routes/payments/controller.php
    - Modified routes/payments/paystack.php

--- routes/payments/paystack.php
+++ routes/payments/paystack.php
@@ -1,6 +1,17 @@
-<?php 
+<?php
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Payments\PaystackPaymentController;
+use App\Http\Kernel;
 
-Route::get('/paystack/callback', [PaystackPaymentController::class, 'handleCallback'])->name('paystack.callback');
+Route::prefix('payment')->name('payment.')->middleware(
+    [
+        Kernel::getAuthMiddleware(),
+        Kernel::getAuthSessionMiddleware(),
+        'onboarding',
+        'intended'
+    ]
+)->group(function () {
+
+    Route::get('/paystack/callback', [PaystackPaymentController::class, 'handleCallback'])->name('paystack.callback');
+});
